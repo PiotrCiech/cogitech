a2cae483d70db0bfe86f8c82e0d2d24b5da9abce
Piotr <piotrekciech@gmail.com>
2025-06-27 17:28:07 +0200

    Zakończenie zadania 3 – dodano metodę GET w integracji z API Platform

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,26 +5,34 @@ namespace App\Entity;
 use App\Repository\PostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(normalizationContext: ['groups' => ['post:read']])]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ORM\Table(name: 'posts')]
 class Post
 {
+    #[Groups(['post:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['post:read'])]
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
+    #[Groups(['post:read'])]
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[Groups(['post:read'])]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $body = null;
 
+    #[Groups(['post:read'])]
     #[ORM\Column(type: 'integer', unique: true)]
     private int $externalId;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,16 +6,19 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'users')]
 class User
 {
+    #[Groups(['post:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['post:read'])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
